1757a781e93dbbcc5a31f4d78441efdee9a8f3a5
Isaiah Thompson <thompson@cs.du.edu>
2023-09-19 00:42:38 -0600

    Built simple function to create pixel data for a checkerboard pattern and added functionality to generate IDAT data. However, it is not saving correctly.

--- pixelRain.py
+++ pixelRain.py
@@ -1,34 +1,54 @@
 import zlib
 
+def subFilter(scanLine):
+    #write sub filter algorithm later
+    return 'ERROR'
+
 def writeToPNG(name,width,height,pixStream):
 
     #establish parameters of the PNG file
-    bitDepth = 8 #bits per color channel
-    colorType = 2 #true color ie RGB
+    bitDepth = 16 #bits per color channel
+    colorType = 6 #truecolor with alpha channel ()
     compressMethod = 0 #0 = no
     filterMethod = 0 #0 = no
     interlaceMethod = 0 #0 = not interlaced
 
     #set up the bytes that will be used to create the PNG image file
-    #standard header to designate PNG file
+
+    #standard header chunk to designate PNG file
     byteHeader = b'\x89\x50\x4E\x47\x0D\x0A\x1A\x0A'
+
     #establish the size of the PNG in pixels
     byteIHDR = width.to_bytes(4)+height.to_bytes(4)
     #convert the parameters established earlier into bytes
     byteIHDR += bitDepth.to_bytes(1)+colorType.to_bytes(1)+compressMethod.to_bytes(1)+filterMethod.to_bytes(1)+interlaceMethod.to_bytes(1)
-    #calculate length of data in chunk
+    #calculate length of chunk data
     IHDRlen = len(byteIHDR)
-    #add IHDR designation to the beginning of the chunk
+    #add chunk type
     byteIHDR = 'IHDR'.encode('ascii')+byteIHDR
-    #add length to beginning of IHDR and calculate CRC of IHDR using zlib
+    #add chunk data length to beginning of IHDR and calculate CRC of chunk type and chunk data using zlib
     byteIHDR = IHDRlen.to_bytes(4)+byteIHDR+zlib.crc32(byteIHDR).to_bytes(4)
-    #byteIHDR = b'\x00\x00\x00\x0D\x49\x48\x44\x52'+width.to_bytes(4, 'big')+height.to_bytes(4, 'big')+b'\x08\x02\x00\x00\x00\x90\x77\x53\xDE'
-    #standard image end chunk
+
+    #filter each scan line from the pixel stream
+    # #Using 0 to signify no filter process for now
+    rawIDAT = b''
+    for scanLine in pixStream:
+        rawIDAT += b'\x00'+scanLine
+    #compress the contiguous stream of filtered scanlines
+    byteIDAT = zlib.compress(rawIDAT)
+    #get length of chunk data
+    IDATlen = len(byteIDAT)
+    #add chunk type
+    byteIDAT = 'IDAT'.encode('ascii')+byteIDAT
+    #add chunk data length to beginning of IDAT and calculate CRC of chunk type and chunk data using zlib
+    byteIDAT = IDATlen.to_bytes(4)+byteIDAT+zlib.crc32(byteIDAT).to_bytes(4)
+
+    #standard PNG image end chunk
     byteIEND = b'\x00\x00\x00\x00\x49\x45\x4E\x44\xAE\x42\x60\x82'
 
     #open/create the PNG file and then write to it
     fileData = open(name+".png","wb")
-    fileData.write(byteHeader+byteIHDR+pixStream+byteIEND)
+    fileData.write(byteHeader+byteIHDR+byteIDAT+byteIEND)
     fileData.close()
 
 def fileWritingTest():
@@ -38,9 +58,6 @@ def fileWritingTest():
     byteIHDR = b'\x00\x00\x00\x0D\x49\x48\x44\x52\x00\x00\x00\x01\x00\x00\x00\x01\x08\x02\x00\x00\x00\x90\x77\x53\xDE'
     byteContent = b'\x00\x00\x00\x0C\x49\x44\x41\x54\x08\xD7\x63\xF8\xCF\xC0\x00\x00\x03\x01\x01\x00\x18\xDD\x8D\xB0'
     byteIEND = b'\x00\x00\x00\x00\x49\x45\x4E\x44\xAE\x42\x60\x82'
-
-    # \x\x\x\x\'
-    # print(byteContent)
 
     #testing ability to create file
     f = open("textTest.txt", "w")
@@ -60,5 +77,29 @@ def fileWritingTest():
     # print(g2.read())
     # g2.close()
 
+def createChecker(width,height): 
+
+    black = b'\x00\x00\x00\xFF'
+    white = b'\xFF\xFF\xFF\xFF'
+
+    board = []
+
+    for row in range(height):
+        if row%2 == 0:
+            currentColor = black
+        else:
+            currentColor = white
+        tempBoardRow = b''
+        for pixel in range(width):
+            tempBoardRow += currentColor
+            currentColor =  bytes(a^b for a, b in zip(currentColor,b'\xFF\xFF\xFF\x00'))
+        board.append(tempBoardRow)
+    
+    return board
+            
 #fileWritingTest()
-writeToPNG("PNGWriteTest",1,1,b'\x00\x00\x00\x0C\x49\x44\x41\x54\x08\xD7\x63\xF8\xCF\xC0\x00\x00\x03\x01\x01\x00\x18\xDD\x8D\xB0')
+#writeToPNG("PNGWriteTest",1,1,b'\x00\x00\x00\x0C\x49\x44\x41\x54\x08\xD7\x63\xF8\xCF\xC0\x00\x00\x03\x01\x01\x00\x18\xDD\x8D\xB0')
+newImg = createChecker(3,3)
+print(newImg)
+writeToPNG("checkerTest", 3, 3, newImg)
+
