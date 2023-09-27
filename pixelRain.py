d12fe96d5dfa31fb72227a76a7cf4073db96998d
Isaiah Thompson <thompson@cs.du.edu>
2023-09-27 00:24:25 -0600

    Started working on the function that will actually create the rain for the PNG.

--- pixelRain.py
+++ pixelRain.py
@@ -1,15 +1,17 @@
 import zlib
 import struct
+import random
 
 def subFilter(scanLine):
     #write sub filter algorithm later
     return 'ERROR'
 
 def scalePixels(factor, pixStream, bitDepth):
+    #this function iterates through each scanline, then takes each pixel and multiplies it by the scaling factor
+    #it then also multiplies each scanline by the scaling factor, saves it to a new pixel stream and returns it
     returnPixels = []
     for scanLine in pixStream:
         tempLine = b''
-        print(str(bitDepth/2)+'s')
         indivBytes = struct.iter_unpack(str(int(bitDepth/2))+'s',scanLine)
         for channel in indivBytes:
             for iter in range(0,factor):
@@ -91,22 +93,30 @@ def fileWritingTest():
     # print(g2.read())
     # g2.close()
 
-def byteColor(pixColor):
+def byteColor8(pixColor):
     color = pixColor[0].to_bytes(1)
     color += pixColor[1].to_bytes(1)
     color += pixColor[2].to_bytes(1)
     color += pixColor[3].to_bytes(1)
     return color
 
+def byteColor16(pixColor):
+    color = pixColor[0].to_bytes(2)
+    color += pixColor[1].to_bytes(2)
+    color += pixColor[2].to_bytes(2)
+    color += pixColor[3].to_bytes(2)
+    return color
+
 def createChecker(name,width,height): 
 
-    black = b'\x00\x00\x00\x00\x00\x00\xFF\xFF'
-    white = b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'
+    black = b'\x00\x00\x00\x00\x00\x00\xFF\xFF'#black pixel bytewise with an alpha channel
+    white = b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'#white pixel bytewise with an alpha channel
     pixelScale = 10 #establish how much bigger I want each checker to be, growth in X and Y
     channelBitDepth = 16 #set bit depth of each channel for final image
 
+    #create the list that will hold all of the rows of the checkerboard
     board = []
-
+    #iterate through each row, then iterate through each square in the row, and alternate between adding a black pixel or a white pixel
     for row in range(height):
         if row%2 == 0:
             currentColor = black
@@ -118,10 +128,32 @@ def createChecker(name,width,height):
             currentColor =  bytes(a^b for a, b in zip(currentColor,b'\xFF\xFF\xFF\xFF\xFF\xFF\x00\x00'))
         board.append(tempBoardRow)
     
+    #scale each pixel up so that it is easier to see the checker pattern
     board = scalePixels(pixelScale,board,channelBitDepth)
+    #send the byte stream of the image to the function that actually formats it as a PNG image
     writeToPNG(name,width*pixelScale,height*pixelScale,board,channelBitDepth)
             
+def createRain(name,width,height):
+
+    #create a list of lists that will hold the pixel data initialized with a clear pixel in every spot
+    rainGrid = [[[0,0,0,0] for _ in range(width)] for _ in range(height)]
+    rainGrid[0][0][3] = 255
+    #create a list of random places on the grid for where we want raindrops for the first frame
+
+    #convert the list of lists into a byte stream that can be used by the PNG writing function
+    pixStream = []
+    for row in rainGrid:
+        tempRow =b''
+        for pixel in row:
+            tempRow+=byteColor8(pixel)
+        pixStream.append(tempRow)
+    print(pixStream)
+    #output the generated image as a PNG
+    writeToPNG(name,width,height,pixStream,8)
+
+
 #fileWritingTest()
 #writeToPNG('PNGWriteTest',1,1,b'\x00\x00\x00\x0C\x49\x44\x41\x54\x08\xD7\x63\xF8\xCF\xC0\x00\x00\x03\x01\x01\x00\x18\xDD\x8D\xB0')
-createChecker('checkerTest',3,3)
+#createChecker('checkerTest',3,3)
 #createChecker('bigCheckerTest',35,50)
+createRain("rainTest",3,5)
